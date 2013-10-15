1140e8fbcc7aa4db26d67bc3d7f23e66d3e1ae83
Davide <davidepi79@gmail.com>
2013-10-15 13:49:57 +0200

    add controller testing

--- client/test/unit/controllersSpec.js
+++ client/test/unit/controllersSpec.js
@@ -2,15 +2,53 @@
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function(){
-  beforeEach(module('myApp.controllers'));
+
+/*beforeEach(function (){module('paudm_db.controllers')});
+describe('PhoneCat controllers', function() {
+
+   it('controller 1 should create db_list 0', inject(function($controller) {
+          var scope = {},
+          ctrl = $controller('MyCtrl2', { $scope: scope });
+          expect(scope.db_list.length).toBe(0);
+    })); 
+    
+ 
+
+ 
+});*/
+
+describe('myApp', function () {
+    var scope,
+        controller,
+        service;
+    beforeEach(function () {
+        module('paudm_db.controllers');
+    });
+    
+    // Mocking service?
+    beforeEach(module(function ($provide) {
+        service = { query: function () {
+                return ['User','Job'];
+            }};        
+        $provide.value('db_list', service);
+    }));
+    
+    describe('MyCtrl1', function () {
+        beforeEach(inject(function ($rootScope, $controller) {
+            scope = $rootScope.$new();
+            controller = $controller('MyCtrl1', {
+                '$scope': scope
+            });
+        }));
+        
+        it('user still not there', function () {
+        	
+        	scope.db_list = service.query();
+            expect(scope.db_list.length).toBe(2);
+        });
 
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
-
-  it('should ....', inject(function() {
-    //spec body
-  }));
+    });
 });
+
+
