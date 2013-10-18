773f573f4b0d6bce5a2574d3415a6b673b57fcf2
Davide <davidepi79@gmail.com>
2013-10-18 10:10:43 +0200

    add default nested view in controller

--- client/app/js/controllers.js
+++ client/app/js/controllers.js
@@ -3,7 +3,7 @@
 /* Controllers */
 
 angular.module('paudm_db.controllers', []).
-  controller('MyCtrl1', ['$scope','db_list', function MyCtrl1($scope, db_list) {
+  controller('MyCtrl1', ['$scope','db_list','$state', function MyCtrl1($scope, db_list, $state) {
   	db_list.query({}, function(data){
 	//1.callback on d3.plot, in the future implement on promise
 	//2. implement callback on failure
@@ -13,6 +13,7 @@ angular.module('paudm_db.controllers', []).
 	
 	});
 	console.log("ccc");
+	$state.go('view1.list');
   }])
    .controller('db_list', ['$scope', function db_list($scope) {
 	$scope.db = {"prova":"db general biew"};
