a7b3d7a47a1ffc1c2e70ed974d6afe079435c75b
Davide <davidepi79@gmail.com>
2013-10-29 10:16:13 +0100

    add dependency directive

--- client/app/js/directives.js
+++ client/app/js/directives.js
@@ -82,70 +82,57 @@ svg.selectAll("rect")
   .directive('d3Force', ['d3', function(d3) {
     return {
       restrict: 'EA',
-    scope: {},
+    scope: {data: '=data',},
       link: function(scope, element, attrs) {
       
       var svg = d3.select(element[0])
             .append("svg")
             .attr("width", "400")
             .attr("height","200");
-      // dummy data
-scope.data = {"nodes":[
-    {"name":"Myriel","group":1},
-    {"name":"Napoleon","group":1},
-    {"name":"Mlle.Baptistine","group":1},
-    {"name":"Mme.Magloire","group":1}],
-    "links":[
-    {"source":1,"target":0,"value":1},
-    {"source":2,"target":0,"value":8},
-    {"source":3,"target":0,"value":10},
-    {"source":3,"target":2,"value":6}
-]};
 
-// on window resize, re-render d3 canvas
-window.onresize = function() {
-  return scope.$apply();
-};
-scope.$watch(function(){
-    return angular.element(window)[0].innerWidth;
-  }, function(){
-    return scope.render(scope.data);
-  }
-);
+
+     // on window resize, re-render d3 canvas
+     window.onresize = function() {
+             return scope.$apply();
+             };
+     scope.$watch(
+           function(){
+              return angular.element(window)[0].innerWidth;
+            }, 
+            function(){
+                return scope.render(scope.data);
+            });
 
 // define render function
-scope.render = function(data){
-
-  scope.force =  d3.layout.force()
-    .charge(-120)
-    .linkDistance(30)
-    .size([400, 400]);
-scope.force
-      .nodes(scope.data.nodes)
-      .links(scope.data.links)
-      .start();
+     scope.render = function(data){
+     scope.force =  d3.layout.force()
+        .charge(-120)
+        .linkDistance(30)
+        .size([400, 400]);
+     scope.force
+        .nodes(scope.data.nodes)
+        .links(scope.data.links)
+        .start();
      var link = svg.selectAll(".link")
-      .data(scope.data.links)
+        .data(scope.data.links)
     .enter().append("line")
-      .attr("class", "link")
-      .style("stroke-width", function(d) { return Math.sqrt(d.value); });
-  var color = d3.scale.category20();
-  var node = svg.selectAll(".node")
-      .data(scope.data.nodes)
-    .enter().append("circle")
-      .attr("class", "node")
-      .attr("r", 5)
-      .style("fill", function(d) { return color(d.group); })
-      .call(scope.force.drag);
-
-  node.append("title")
-      .text(function(d) { return d.name; });
-
-  scope.force.on("tick", function() {
-    link.attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+        .attr("class", "link")
+        .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+     var color = d3.scale.category20();
+     var node = svg.selectAll(".node")
+        .data(scope.data.nodes)
+        .enter().append("circle")
+        .attr("class", "node")
+        .attr("r", 5)
+        .style("fill", function(d) { return color(d.group); })
+        .call(scope.force.drag);
+    node.append("title")
+        .text(function(d) { return d.name; });
+    scope.force.on("tick", function() {
+         link.attr("x1", function(d) { return d.source.x; })
+         .attr("y1", function(d) { return d.source.y; })
+         .attr("x2", function(d) { return d.target.x; })
+         .attr("y2", function(d) { return d.target.y; });
 
     node.attr("cx", function(d) { return d.x; })
         .attr("cy", function(d) { return d.y; });
