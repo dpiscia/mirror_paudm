b3e5074f037d59957a1204d541f911f2b29dadce
Davide <davidepi79@gmail.com>
2013-10-29 15:03:44 +0100

    DDd

--- client/config/karma.conf.js
+++ client/config/karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function(config){
 
     frameworks: ['jasmine'],
 
-    browsers : ['Chrome'],
+    browsers : ['Firefox'],
 
     plugins : [
             'karma-junit-reporter',
