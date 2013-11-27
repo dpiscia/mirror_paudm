0f5ee51bbedf6794138af70b06f91a5ad994c053
Davide <davidepi79@gmail.com>
2013-11-27 08:05:05 +0100

    fix

--- client/app/job_monitor/job_controller.js
+++ client/app/job_monitor/job_controller.js
@@ -3,7 +3,7 @@ angular.module('job_monitor.single_job', [])
 		$scope.job = job[0];
 		$scope.quality_controls = QC;	
 		window.qc = "";
-		$http({method: "GET", url: "http://localhost:3000/js/qc_constants.py"}).success(function(data){$scope.info = eval("(" +data+ ')'); });
+		$http({method: "GET", url: "http://localhost:3000/qc_constants.py"}).success(function(data){$scope.info = eval("(" +data+ ')'); });
 		$scope.open = function (name) {
  			 	$scope.img = name;
 				var modalInstance = $modal.open({
