03546b65c2921cae007417aef31d4d491bed46d1
Davide <davidepi79@gmail.com>
2013-12-20 12:20:43 +0100

    some refactoring

--- api_python/api_python/views.py
+++ api_python/api_python/views.py
@@ -1,20 +1,22 @@
 """ Cornice services.
 """
 from cornice import Service
-from paudm.tools.db import model #in the future it will be imported by other packages
-from sqlalchemy import Table
-
+from sqlalchemy import inspect, create_engine
+import api_python 
 tb_list = Service(name='db_list', path='/db_list', description="Simplest app")
 fields_list = Service(name='fields_list', path='/tb/{table}', description="Simplest app")
 
+        
+        
 @tb_list.get()
 def get(request):
-	tables = model.metadata.tables.keys()
+	#import pdb;  pdb.set_trace()
+	tables = api_python.insp.get_table_names()
 	result = []
 	for table in tables:
 		for_keys = []
-		for c in Table(table,model.metadata, autoload=True).foreign_keys:
-			 for_keys.append(c.target_fullname)
+		for c in api_python.insp.get_foreign_keys(table):
+			 for_keys.append(c)
 		result.append({"name" : table, "foreign_keys" : for_keys })
 	return   result
 	
@@ -25,5 +27,7 @@ def get(request):
 	#def get(self,tb_name):
 	tb_name = request.matchdict['table']
 	
-	table = Table(tb_name, model.metadata, autoload=True)	
-	return  {'list' : [c.name for c in table.columns] } 
+		
+	return  {'list' :  {'columns' : [c['name'] for c in api_python.insp.get_columns(tb_name)], 
+						'indexes' : {'name' :[c['name'] for c in api_python.insp.get_indexes(tb_name)] , 'column_names' :[c['column_names'] for c in api_python.insp.get_indexes(tb_name)]}, 
+						'pr_keys' : [c for c in api_python.insp.get_primary_keys(tb_name)]} } 
