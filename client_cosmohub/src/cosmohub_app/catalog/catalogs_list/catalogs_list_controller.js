7bbbf3ea28b5ec7ee35a6781ea024f084ad47635
piscia@ieec.uab.es <dpiscia@services.astro.pic.es>
2014-02-11 15:08:19 +0100

    d

--- client_cosmohub/src/cosmohub_app/catalog/catalogs_list/catalogs_list_controller.js
+++
@@ -1,4 +0,0 @@
-angular.module('catalogs.controllers',['ui.bootstrap']).controller('catalogs_list_ctrl', [ '$scope','catalogs','user_auth','$location',  function ($scope, catalogs, user_auth, $location) {
-   		$scope.catalogs = catalogs.catalogs;
-   		
-}]);
--- client_cosmohub/src/cosmohub_app/catalog/catalogs_list/single_catalog/single_catalog_controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/* Controllers */
-
-angular.module('catalogs.controllers').controller('single_catalog_ctrl', [ '$scope','catalog','user_auth','$location', function ($scope, catalog, user_auth, $location) {
-   		$scope.catalog = catalog;
-
-   		
-}]);
